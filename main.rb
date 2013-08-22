ef5c89e4f005ee71396426e68eb6b28147e3ca79
marvinick <marviluw@gmail.com>
2013-08-22 14:48:19 -0500

    updated with card images

--- main.rb
+++ main.rb
@@ -25,7 +25,30 @@ helpers do
   
   	total 
 	end 
+
+	def card_image(card) 
+		suit = case card [0]
+			when 'H' then 'hearts'
+			when 'D' then 'diamonds'
+			when 'C' then 'clubs'
+			when 'S' then 'spades' 
+		end 
+
+		value = card[1]
+		if['J','Q','K','J'].include?(value)
+			value = case card[1]
+				when 'J' then 'jack'
+				when 'Q' then 'queen'
+				when 'K' then 'king'
+				when 'A' then 'ace'
+			end
+		end
+
+		"<img src='/images/cards/#{suit}_#{value}.jpg' class='card_image'>"
+	end 
 end 
+
+
 
 before do 
 	@show_hit_or_stay_buttons = true 
@@ -67,8 +90,12 @@ end
 
 post '/game/player/hit' do 
 	session[:player_cards] << session[:deck].pop 
-	if calculate_total(session[:player_cards]) > 21 
-		@error = "You are busted!."
+
+	player_total = calculate_total(session[:player_cards])
+	if player_total == 21 
+		@success = "Congratulations! #{session[:player_name]} hit BLACKJACK!!!"
+	elsif player_total > 21 
+		@error = "#{session[:player_name]} is busted!"
 		@show_hit_or_stay_buttons = false 
 	end 
 
@@ -76,7 +103,7 @@ post '/game/player/hit' do
 end 
 
 post '/game/player/stay' do 
-	@success = "You opted to stay." 
+	@success = "#{session[:player_name]} opted to stay!" 
 	@show_hit_or_stay_button = false 
 	erb :game 
 end 
