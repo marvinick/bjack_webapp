029a50a9add95e896841193c09ceceb6765708d6
marvinick <marviluw@gmail.com>
2013-08-28 00:52:03 -0500

    ajax functions added

--- main.rb
+++ main.rb
@@ -54,20 +54,20 @@ helpers do
 	  @play_again = true
 	  @show_hit_or_stay_buttons = false
 	  session[:player_pot] = session[:player_pot] + session[:player_bet]
-	  @success = "<strong>#{session[:player_name]} WINS!</strong> #{msg}"
+	  @winner = "<strong>#{session[:player_name]} WINS!</strong> #{msg}"
   end  
 
   def loser!(msg)
 	  @play_again = true
 	  @show_hit_or_stay_buttons = false
 	  session[:player_pot] = session[:player_pot] - session[:player_bet]
-	  @error = "<strong>#{session[:player_name]} LOSES!</strong> #{msg}"
+	  @loser = "<strong>#{session[:player_name]} LOSES!</strong> #{msg}"
   end 
 
   def tie!(msg)
 	  @play_again = true
 	  @show_hit_or_stay_buttons = false
-	  @success = "<strong> It's a TIE!</strong> #{msg}"
+	  @winner = "<strong> It's a TIE!</strong> #{msg}"
   end 
 end 
 
@@ -145,7 +145,7 @@ post '/game/player/hit' do
 	  loser!("It looks like #{session[:player_name]} busted at #{player_total}.") 
   end 
 
-  erb :game
+  erb :game, layout: false  
 end 
 
 post '/game/player/stay' do 
@@ -173,7 +173,7 @@ get '/game/dealer' do
 	  @show_dealer_hit_button = true 
   end 
 
-  erb :game
+  erb :game, layout: false
 end 
 
 post '/game/dealer/hit' do 
@@ -195,7 +195,7 @@ get '/game/compare' do
 	  tie!("Both  #{session[:player_name]} and the dealer stayed at #{player_total}")
   end 
 
-  erb :game 
+  erb :game, layout: false
 end 
 
 get '/game_over' do 
